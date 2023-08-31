6d0c76c13f7f27827003184dbd71eb987a2ce056
richie nolan <richienolan23@gmail.com>
2023-08-31 17:12:09 +0000

    add js structure

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+function runGame() 
+
+function checkAnswer() 
+
+function calculateCorrectAnswer() 
+
+function incrementScore() 
+
+function incrementWrongAnswer()
+
+function displayAdditionQuestion() 
+
+function displaySubtractQuestion()
+
+function diplayMultiplyQuestion()
+
+function displayDivisionQuestion()
