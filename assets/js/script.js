20cd842ef27267e9170501d6db26b872f645d861
richie nolan <richienolan23@gmail.com>
2023-09-04 16:37:28 +0000

    add code to checkanswer and calculate correct answer fucntions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("you clicked submit");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType)
@@ -29,11 +29,28 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got right!");
+    } else {
+        alert(`you answered  ${userAnswer}. the correct answer was ${calculatedAnswer[0]}`);
+    }
 }
 
 function calculateCorrectAnswer() { 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `unimplemented operator ${operator}. aborting!`
+    }
 }
 
 function incrementScore() {
