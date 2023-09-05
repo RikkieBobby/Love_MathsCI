b828b6efd4ac1ff0a62e6e0e30a4ebce068b56e9
richie nolan <richienolan23@gmail.com>
2023-09-05 16:39:39 +0000

    add comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
@@ -21,6 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
     runGame("addition")
 }) 
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
 function runGame(gameType) {
 
@@ -44,6 +51,11 @@ function runGame(gameType) {
     }
 }
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+
 function checkAnswer() {
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
@@ -57,6 +69,11 @@ function checkAnswer() {
         incrementWrongAnswer();
     }
 }
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 
 function calculateCorrectAnswer() { 
     let operand1 = parseInt(document.getElementById('operand1').innerText);
@@ -77,10 +94,18 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
 function incrementScore() {
      let oldScore = parseInt(document.getElementById("score").innerText); 
      document.getElementById("score").innerText = ++oldScore;
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 
 function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText); 
